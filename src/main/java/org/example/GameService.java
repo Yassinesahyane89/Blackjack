03c623a6bd0c06ca4c0a201ce4e3c96f0807a77a
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-10-02 11:19:10 +0100

    add function CreateSubDeck that take a card and return all card that following && function SuitsElementIndex

--- src/main/java/org/example/GameService.java
+++ src/main/java/org/example/GameService.java
@@ -14,4 +14,36 @@ public class GameService {
         }
         return deck;
     }
+
+    public static int ElementIndex(String suit){
+        int elementIndex = -1;
+        String[] suits = CardConstants.suits;
+        for (int i = 0; i < suits.length; i++) {
+            if(suit.equals(suits[i])){
+                elementIndex = i;
+            }
+        }
+
+        return elementIndex;
+    }
+
+    public static Card[] CreateSubDeck(Card startingCard){
+
+        int suitIndex = ElementIndex(startingCard.getSuit());
+        int lengthDeck = ((CardConstants.suits.length-1) - suitIndex )*13 + 13 - startingCard.getRange();
+
+        Card[] deck = new Card[lengthDeck];
+
+        int cardIndex = 0;
+        int startingCardRange = startingCard.getRange();
+        for (int i = suitIndex; i <4 ; i++) {
+            for (int j = startingCardRange; j < 13; j++) {
+                deck[cardIndex] = new Card(j+1,CardConstants.suits[i]);
+                cardIndex++;
+            }
+            startingCardRange=0;
+        }
+
+        return deck;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-        Card[] deck= GameService.CreateDeck();
+        Card[] deck= GameService.CreateSubDeck(new Card(4,"Diamonds"));
 
         for (Card card: deck) {
             System.out.println("[ " + card.getRange() + "," + card.getSuit() + " ]");
