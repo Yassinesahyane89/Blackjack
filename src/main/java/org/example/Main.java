99730734df2c55c1f117c3145217d9c36793b315
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-10-06 18:17:58 +0100

    add logique for let player enter his name and money in Main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,32 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Player player = new Player();
 
+        /**
+         * Enter Player name
+         */
+        System.out.print("Enter your name : ");
+        String playerName = sc.nextLine();
+        while (playerName.isEmpty() || playerName.matches("^[a-zA-Z0-9]*$")){
+            System.out.println("Invalid name. Please use only letters and numbers.");
+            System.out.print("Enter your name : ");
+            playerName = sc.nextLine();
+        }
+
+        /**
+         * Enter Player Money.
+         */
+        player.setName(playerName);
+        System.out.print("Enter how much money you have: ");
+        int playerMoney = sc.nextInt();
+        while (playerMoney<=0){
+            System.out.println("Invalid money amount. Please enter a non-negative integer.");
+            System.out.print("Enter how much money you have: ");
+            playerMoney = sc.nextInt();
+        }
+        player.setMoney(playerMoney);
     }
 
 }
