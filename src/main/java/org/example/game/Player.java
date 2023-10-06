4e0858c482e22c8d3ee7c64837a3496de605717a
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-10-06 18:17:18 +0100

    add attribut in Player class

--- src/main/java/org/example/game/Player.java
+++ src/main/java/org/example/game/Player.java
@@ -1,4 +1,43 @@
 package org.example.game;
 
+import lombok.Data;
+
+@Data
 public class Player {
+    /**
+     * Player's name.
+     */
+    private String name;
+    /**
+     * Player's money.
+     */
+    private int money;
+    /**
+     * Player's bet.
+     */
+    private int bet;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public int getBet() {
+        return bet;
+    }
+
+    public void setBet(int bet) {
+        this.bet = bet;
+    }
 }
